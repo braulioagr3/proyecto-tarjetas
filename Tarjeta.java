b5c3f9ba82b871a777083aca21501e1b140923e0
Braulio García <braulioagr3@gmail.com>
2017-08-31 21:32:01 -0500

    Se añadio la documentacion con JavaDoc

--- Tarjeta.java
+++ Tarjeta.java
@@ -7,18 +7,27 @@ public class Tarjeta
         // initialise instance variables
         Saldo = 0;
     }
+    /**
+     * Mediante validaciones permite insertar saldo y evitando que el usuario
+     * ingrese saldo negativo
+     */
     public void Depositar(float Dinero)
     {
         if(Dinero > 0 && Saldo > 0){
             Saldo = Saldo + Dinero;
-            System.out.println("Saldo no Valido");
         }
     }
-    public void Consultar()
+    /**
+     * Permite consultar el saldo
+     * @return Saldo actual del usuario
+     */
+    public float Consultar()
     {
-        System.out.println("Su Saldo es: ");
-        System.out.println(Saldo);
+        return Saldo;
     }
+    /**
+     * Permite hacer retiros de saldo
+     */
     public void Retirar(float Dinero)
     {
         if(Saldo >=Dinero)
